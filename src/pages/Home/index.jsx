6a3a8e808da77e94ab12535385b5b98d549eafc8
Maxence Cool <max-game10@live.fr>
2024-04-26 12:50:46 +0200

    Modification de la page d'accueil

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,8 +4,12 @@ function Home() {
   return (
     <main>
       <div className="Presentation">
-        <h1 className="title">PRESENTATION</h1>
-        <div className="information">Blabla blabla bla ....</div>
+        <h1 className="Title">Présentation</h1>
+        <div className="Information">
+          Bienvenue sur mon site de présentation de mon portfolio développeur.
+          Vous y trouverez des informations sur moi ainsi que mes réalisations,
+          accessible directement depuis la barre de navigation
+        </div>
       </div>
     </main>
   );
