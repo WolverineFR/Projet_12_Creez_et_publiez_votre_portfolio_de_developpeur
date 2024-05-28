7505046855ecfb15f2478486f838b3675e552154
Maxence Cool <max-game10@live.fr>
2024-05-28 12:57:38 +0200

    page A propos fini

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ProfilePicture from "../../assets/profile.webp";
+import LaptopPicture from "../../assets/laptop-pic.webp";
+import SkillsPicture from "../../assets/skills.webp";
+import BookPicture from "../../assets/book.webp";
 
 function Apropos() {
   return (
     <main>
       <div className="AproposSection">
-        <div className="Presentation">
+        <div className="PresentationApropos">
           <div className="TitleBox">
             <h1 className="Title">A propos de moi</h1>
             <div className="UnderlineTitle"></div>
@@ -37,12 +40,39 @@ function Apropos() {
           <div className="greyLine"></div>
         </div>
         <div className="AproposSectionTwo">
+          <img className="LaptopPicture" alt="Laptop" src={LaptopPicture}></img>
+          <div className="greyLine"></div>
           <div className="TextSectionTwo">
-            Mon voyage dans le monde du développement a commencé avec une
-            curiosité sans fin pour comprendre comment les sites web prennent
-            vie et évoluent pour répondre aux besoins des utilisateurs.
+            Au fil de mes études et de mes projets personnels, j'ai acquis une
+            solide base en langages de programmation tels que HTML, CSS et
+            JavaScript, ainsi qu'une compréhension approfondie des frameworks et
+            bibliothèques populaires comme React. Mon expérience pratique m'a
+            permis de maîtriser les principes du développement front-end et de
+            créer des interfaces utilisateur interactives et esthétiques.
           </div>
           <div className="greyLine"></div>
+        </div>
+        <div className="AproposSectionThree">
+          <div className="AproposSectionThreeBox">
+            <img
+              className="SkillsPicture"
+              alt="Skills"
+              src={SkillsPicture}
+            ></img>
+            <div className="TextSectionThree1">
+              En tant que développeur web junior, je suis constamment à la
+              recherche de nouvelles opportunités pour élargir mes compétences
+              et relever des défis stimulants.
+            </div>
+
+            <div className="TextSectionThree2">
+              Je suis passionné par l'apprentissage continu et je m'efforce
+              d'approfondir mes connaissances en matière de développement web en
+              suivant les dernières tendances et en participant à des projets
+              collaboratifs.
+            </div>
+            <img className="BookPicture" alt="Book" src={BookPicture}></img>
+          </div>
         </div>
       </div>
     </main>
