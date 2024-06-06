341eeca288395eb861f4aa89eb1becf8c041b601
Maxence Cool <max-game10@live.fr>
2024-06-06 12:11:30 +0200

    mise en place du texte descriptif pour les cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import { useSelector } from "react-redux";
-// import { Navigate } from "react-router-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,6 +5,10 @@ import { Link } from "react-router-dom";
 const Card = ({ data }) => {
   return (
     <Link to={data.lien} className="Card" href="" key={data.id}>
+      <div className="DescriptionCompetenceBox">
+        <p className="description">{data.description}</p>
+        <p className="competences">{data.competences}</p>
+      </div>
       <img className="cardImg" alt={data.title} src={data.picture}></img>
       <p className="cardTitle">{data.title}</p>
     </Link>
