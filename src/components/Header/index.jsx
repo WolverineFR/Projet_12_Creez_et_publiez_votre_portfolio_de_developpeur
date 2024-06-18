2284d73f0c851ec2ddda0e0536fdf0ba82317c2e
Maxence Cool <max-game10@live.fr>
2024-06-18 15:11:16 +0200

    modification de la barre de navigation du header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,23 +7,36 @@ function Header() {
       <NavLink to="/" className="logo" href="">
         CLMX
       </NavLink>
-      <div className="Navigation-Box">
-        <NavLink to="/" className="Accueil">
-          Accueil
-        </NavLink>
-        <NavLink to="/A-propos" className="Apropos" href="">
-          À Propos
-        </NavLink>
-        <NavLink to="/Realisations" className="Realisations" href="">
-          Réalisations
-        </NavLink>
-        <NavLink to="/Competences" className="Competences" href="">
-          Compétences
-        </NavLink>
-        <NavLink to="/Contact" className="Contact" href="">
-          Contact
-        </NavLink>
-      </div>
+      <nav className="Navigation-Box">
+        <div className="DropdownMenu">☰</div>
+        <ul>
+          <li>
+            <NavLink to="/" className="Accueil">
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/A-propos" className="Apropos" href="">
+              À Propos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Realisations" className="Realisations" href="">
+              Réalisations
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Competences" className="Competences" href="">
+              Compétences
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Contact" className="Contact" href="">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
